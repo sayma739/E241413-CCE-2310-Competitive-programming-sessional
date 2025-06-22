6026b35af5a58741df900455baa87c4e8e9976a0
sayma739 <tasniasultanajune@gmail.com>
2025-06-23 00:14:36 +0600

    Add files via upload

---
+++ Sheet 3(Arrays)/prob i.cpp
@@ -0,0 +1,34 @@
+// Problem Name :  Smallest Pair
+//Course Title : Competitive Programming Sessional
+//Course Code : CCE-2310
+//Course Teacher Name : Mirza Raquib
+//Date : 16/06/25
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long t;
+cin>>t;
+while(t--)
+    {
+     long long n;
+     cin>>n;
+     long long a[n+1];
+
+for(int i=1;i<=n;i++)
+  {
+    cin>>a[i];
+  }
+  long long sum=10000000;
+  for(long long i=1;i<=n-1;i++)
+  {
+      for(long long j=i+1;j<=n;j++)
+      {
+          long long ans=a[i]+a[j]+j-i;
+          if(ans<sum)
+            sum=ans;
+     }
+  }
+  cout<<sum<<endl;
+    }
+}
